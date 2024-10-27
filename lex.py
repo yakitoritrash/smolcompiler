49fc7a5a5074444227a752f9fbb7b959da4010d1
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2024-10-27 20:30:44 +0530

    lexer

---
+++ lex.py
@@ -0,0 +1,29 @@
+class Lexer():
+    def __init__(self, source):
+        self.source = source + '\n'
+        self.curChar = ''
+        self.curPos = -1
+        self.nextChar()
+    
+    def nextChar(self):
+        self.curPos += 1
+        if self.curPos >= len(self.source):
+            self.curChar = '\0'
+        else:
+            self.curChar = self.source[self.curPos]
+    
+    def peek(self):
+        pass
+    
+    def abort(self, message):
+        pass
+    
+    def skipWhitespace(self):
+        pass
+    
+    def skipComment(skip):
+        pass
+    
+    def getToken(self):
+        pass 
+    

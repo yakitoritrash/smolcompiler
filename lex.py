411f70127fa7b7577662c38dfb9b34d2d1549e87
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2024-10-28 15:45:18 +0530

    lexer

--- lex.py
+++ lex.py
@@ -1,3 +1,5 @@
+import enum
+
 class Lexer():
     def __init__(self, source):
         self.source = source + '\n'
@@ -27,5 +29,56 @@ class Lexer():
         pass
     
     def getToken(self):
-        pass 
+        if self.curChar == '+':
+            pass
+        elif self.curChar == '-':
+            pass
+        elif self.curChar == '*':
+            pass
+        elif self.curChar == '/':
+            pass
+        elif self.curChar == '\n':
+            pass
+        elif self.curChar == '\0':
+            pass
+        else:
+            pass
+        
+        self.nextChar() 
+        
+class Token:
+    def __init__(self, tokenText, tokenKind):
+        self.text = tokenText
+        self.kind = tokenKind
+        
     
+class TokenType(enum.Enum):
+    EOF = -1
+    NEWLINE = 0
+    NUMBER = 1
+    IDENT = 2
+    STRING = 3
+    
+    LABEL = 101
+    GOTO = 102
+    PRINT = 103
+    INPUT = 104
+    LET = 105
+    IF = 106
+    THEN = 107
+    ENDIF = 108
+    WHILE = 109
+    REPEAT = 110
+    ENDWHILE = 111
+    
+    EQ = 201
+    PLUS = 202
+    MINUS = 203
+    ASTERISK = 204
+    SLASH = 205
+    EQEQ = 206
+    NOTEQ = 207
+    LT = 208
+    LTEQ = 209
+    GT = 210
+    GTEQ = 211

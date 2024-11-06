631949cfeca9e63feb6160758bf00fee4dccf762
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2024-11-06 21:27:52 +0530

    alpha

--- lex.py
+++ lex.py
@@ -125,6 +125,13 @@ class Token:
     def __init__(self, tokenText, tokenKind):
         self.text = tokenText
         self.kind = tokenKind
+    
+    @staticmethod
+    def checkIfKeyword(tokenText):
+        for kind in TokenType:
+            if kind.name == tokenText and kind.value >= 100 and kind.value < 200:
+                return kind
+        return None
         
     
 class TokenType(enum.Enum):
